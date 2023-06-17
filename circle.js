0b3570956d85eb734d34e8913d9b81c0fdcde41b
Vlad21Islav <vmitk@mail.ru>
2023-06-17 20:59:03 +0300

    circle

--- circle.js
+++ circle.js
@@ -10,7 +10,9 @@ const rl = readline.createInterface({
 const P = 3.14
 
 rl.question('Введите радиус окружности в сантиметрах: ', (R) => {
-  console.log(`Площадь круга: ${P*R*2} см.кв.`);
-  console.log(`Длина окружности: ${2*P*R} см.`);
+  if (R === Number) {
+    console.log(`Площадь круга: ${P*R*2} см.кв.`);
+    console.log(`Длина окружности: ${2*P*R} см.`);
+  }
   rl.close();
 });
