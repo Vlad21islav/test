7e738da7b9981f06ceb90fba6857d751bbe96ca2
Vlad21Islav <vmitk@mail.ru>
2023-06-17 18:43:12 +0300

    circle.js

--- test.js
+++ test.js
@@ -67,3 +67,83 @@ const generateKey = (alphabet) => {
 generateKey('159236159236az')('159236159236az'.length)
 
 color(42)('hello world')
+
+const person = {
+  name: '',
+  age: '',
+  phone: '',
+  logInfo: function(job = '') {
+    console.group(`${this.name}'s info:`)
+    console.log(`Name is ${this.name}`)
+    console.log(`Age is ${this.age}`)
+    console.log(`phone is ${this.phone}`)
+    if (job !== '') console.log(`job is ${job}`)
+    console.groupEnd()
+  }
+}
+
+const Vlad = {
+  name: 'Vladislav',
+  age: '13',
+  phone: '*-***-***-**-**'
+}
+
+person.logInfo.call(Vlad)
+
+function bind(person, log) {
+  log.apply(person, [log])
+}
+
+function logPerson() {
+  console.log(`person: ${this.name},\n${this.age},\n${this.job}`)
+}
+
+const person1 = {name: 'Михаил', age: 22, job: 'Frontend'}
+const person2 = {name: 'Елена', age: 19, job: 'SMM'}
+
+bind(person2, logPerson)
+
+const sleep = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), ms)
+  })
+} 
+
+sleep(2000).then(() => console.log('hello1'))
+console.log('hello2')
+
+class Animal {
+  static type = 'ANIMAL'
+
+  constructor(options) {
+    this.name = options.name
+    this.hasTail = options.hasTail
+  }
+
+  voice() {
+    console.log("I'm animal")
+  }
+
+}
+
+class Cat extends Animal {
+  static type = 'CAT'
+
+  constructor(options) {
+    super(options)
+    this.color = options.color
+  }
+
+  voice() {
+    super.voice()
+    console.log("I'm cat")
+  }
+
+}
+
+const cat = new Cat({
+  name: 'Cat',
+  hasTail: true,
+  color: 'black'
+})
+
