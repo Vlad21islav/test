30319047cec48782bab9b7d9982c0e7c5f70bf12
Vlad21Islav <vmitk@mail.ru>
2023-06-18 10:35:22 +0300

    circle

--- tasks/circle/circle.js
+++ tasks/circle/circle.js
@@ -7,14 +7,22 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const P = 3.14
+const P = Math.PI
 
-rl.question('Введите радиус окружности в сантиметрах: ', (R) => {
-  if (isFinite(R)) {
-    console.log(`Площадь круга: ${P*R*2} см.кв.`);
-    console.log(`Длина окружности: ${2*P*R} см.`);
+const length = (R) => {
+  return Math.round((2*P*R), 3)
+}
+
+const square = (R) => {
+  return Math.round((P*(Math.pow(R, 2))), 3)
+}
+
+rl.question('Введите радиус окружности в сантиметрах: ', (Radius) => {
+  if (isFinite(Radius)) {
+    console.log(`Площадь круга: ${square(Radius)} см.кв.`);
+    console.log(`Длина окружности: ${length(Radius)} см.`);
   }
-  else {console.log(`${R} - это не число`)
+  else {console.log(`${Radius} - это не число`)
   }
   rl.close();
 });
