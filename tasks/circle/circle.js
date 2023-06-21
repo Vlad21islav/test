2ff040a1abee0b283d403c1e367a376f5bbb804a
Vlad21Islav <vmitk@mail.ru>
2023-06-21 15:49:01 +0300

    if/else, new consts

--- tasks/circle/circle.js
+++ tasks/circle/circle.js
@@ -12,10 +12,10 @@ const calculateTheCircumference = (radius) => (2*Math.PI*radius);
 const calculateTheAreaOfTheCircle = (radius) => (Math.PI*(Math.pow(radius, 2)));
 
 rl.question('Введите радиус окружности в сантиметрах: ', (message) => {
-  const radius = Number(message)
+  const radius = Number(message);
   if (!Number.isNaN(radius)) {
-    const circumference = calculateTheCircumference(radius)
-    const area = calculateTheAreaOfTheCircle(radius)
+    const circumference = calculateTheCircumference(radius);
+    const area = calculateTheAreaOfTheCircle(radius);
     console.log(`Площадь круга: ${circumference.toFixed(2)} см.кв.`);
     console.log(`Длина окружности: ${area.toFixed(2)} см.`);
   }else console.log(`${message} - это не число`);
