93af6c2d47b26373d4aa349ba1d8e5623427503c
Vlad21Islav <vmitk@mail.ru>
2023-07-03 16:10:52 +0300

    add

--- tasks/login/functions/setName.js
+++ tasks/login/functions/setName.js
@@ -4,9 +4,9 @@ const users = require('./users')
 function setName(line) {
   const user = users.find((element) => (element.login === line));  
   if (user === undefined) {
-    const {add} = getState(); 
+    const add = { login: line }; 
     add.login = line;
-    setState({status: 'addingPassword', user});
+    setState({status: 'addingPassword', add});
     console.log(`Введите пароль: `);
   } else {
     console.log(`Такой пользователь уже есть: `);
