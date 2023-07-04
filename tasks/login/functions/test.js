11fc38f63cd52010b45fb92484c6aad33963222e
Vlad21Islav <vmitk@mail.ru>
2023-07-04 20:11:11 +0300

    shift

--- tasks/login/functions/test.js
+++ tasks/login/functions/test.js
@@ -1,14 +1,15 @@
 function paraming(params) {
     paramObject = {}
     let splitParams = params.split(' ')
-        for (let param of splitParams) {
-            let paramsKey = param.split('=')
-            if (paramsKey.length === 2) {
-                let key = paramsKey[0]
-                let value = paramsKey[1]
-                paramObject[key] = value
-            }
+    splitParams.shift() 
+    for (let param of splitParams) {
+        let paramsKey = param.split('=')
+        if (paramsKey.length === 2) {
+            let key = paramsKey[0]
+            let value = paramsKey[1]
+            paramObject[key] = value
         }
+    }
     return paramObject
 }
 
