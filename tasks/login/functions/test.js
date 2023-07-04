ee489ddb47e9beaab45108c539c99b515586a0d0
Vlad21Islav <vmitk@mail.ru>
2023-07-04 18:48:54 +0300

    test

--- tasks/login/functions/test.js
+++ tasks/login/functions/test.js
@@ -1,9 +1,26 @@
-function paraming(...params) {
+function paraming(params) {
     paramObject = {}
-    for (let param in params) {
-        paramObject.param = params[param]
+    let splitParams = params.split(' ')
+    let splitParamsLen = splitParams.filter(() => true).length;
+    if (splitParamsLen === 3) {
+        if (splitParams[0] === 'add') {
+            let paramsKey1 = splitParams[1].split('=')
+            let paramsKey1Len = paramsKey1.filter(() => true).length;
+            if (paramsKey1Len === 2) {
+                if (paramsKey1[0] === 'login') {
+                    paramObject.login = paramsKey1[1]
+                    let paramsKey2 = splitParams[2].split('=')
+                    let paramsKey2Len = paramsKey2.filter(() => true).length;
+                    if (paramsKey2Len === 2) {
+                        if (paramsKey1[0] === 'password') {
+                            paramObject.password = paramsKey2[1]
+                        }
+                    }
+                }
+            }
+        }
     }
     return paramObject
 }
 
-console.log(paraming('hi', 'hello'))
+console.log(paraming('add login=Привет😂 password=Приветович🤣'))
