6d38c72de55d3de54e2517627fa861c8cf782540
Vlad21Islav <vmitk@mail.ru>
2023-07-04 19:04:22 +0300

    over

--- tasks/login/functions/test.js
+++ tasks/login/functions/test.js
@@ -4,17 +4,19 @@ function paraming(params) {
     let splitParams = params.split(' ')
     let splitParamsLen = splitParams.filter(() => true).length;
     if (splitParamsLen === 3) {
-        if (splitParams[0] === 'add') {
-            let paramsKey1 = splitParams[1].split('=')
+        const isAdd = splitParams.shift() 
+        if (isAdd === 'add') {
+            let paramsKey1 = splitParams[0].split('=')
             let paramsKey1Len = paramsKey1.filter(() => true).length;
             if (paramsKey1Len === 2) {
                 if (paramsKey1[0] === 'login') {
                     paramObject.login = paramsKey1[1]
-                    let paramsKey2 = splitParams[2].split('=')
+                    let paramsKey2 = splitParams[1].split('=')
                     let paramsKey2Len = paramsKey2.filter(() => true).length;
                     if (paramsKey2Len === 2) {
                         if (paramsKey2[0] === 'password') {
                             paramObject.password = paramsKey2[1]
+                            over = true
                         }
                     }
                 }
@@ -28,4 +30,4 @@ function paraming(params) {
     }
 }
 
-console.log(paraming('add logn=Привет😂 password=Приветович🤣'))
+console.log(paraming('add login=Привет😂 password=Приветович🤣'))
