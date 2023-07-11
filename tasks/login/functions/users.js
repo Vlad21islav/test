ccad87263e6a2ddd34aa8acf24187e3b832bb086
Vlad21Islav <vmitk@mail.ru>
2023-07-11 14:58:31 +0300

    users

--- tasks/login/functions/users.js
+++ tasks/login/functions/users.js
@@ -1,6 +1,14 @@
 const users = [
     { login: 'Vlad', password: '1324' },
     { login: 'Alex', password: '2546' },
+    { login: 'Dima', password: '1234' },
+    { login: 'Misha', password: '4567' },
+    { login: 'Masha', password: '8765' },
+    { login: 'Ivan', password: '9876' },
+    { login: 'Tanya', password: '0987' },
+    { login: 'Zhenya', password: '5498' },
+    { login: 'Ann', password: '8056' },
+    { login: 'Irina', password: '7409' },
 ]
 
 module.exports = users;
