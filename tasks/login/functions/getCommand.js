04b9867755a133b1027a5483c77371bd63c06a32
Vlad21Islav <vmitk@mail.ru>
2023-07-14 11:08:34 +0300

    add, delete, list

--- tasks/login/functions/getCommand.js
+++ tasks/login/functions/getCommand.js
@@ -2,64 +2,26 @@ const {getState} = require('./stateManager')
 const getActionParams = require('./getActionParams')
 const users = require('./users')
 
+const addCommand = require('./commands/add')
+const clearCommand = require('./commands/clear')
+const deleteCommand = require('./commands/add')
+const exitCommand = require('./commands/exit')
+const helpCommand = require('./commands/help')
+const listCommand = require('./commands/list')
+
 function commands(line) {
   const {action, params} = getActionParams(line)
   switch (action) {
     case 'add':
-      if (params.login === undefined || params.password === undefined) {
-        console.log('Вы ввели не правильную команду');
-        break;
-      };
-      const findUser = users.find((element) => (element.login === params.login));
-      if (findUser !== undefined) {
-        console.log('Такой пользователь уже есть');
-        break;
-      };
-      users.push(params)
-      console.log(`${params.login} добавлен(a)`);
+      addCommand(params);
       break;
 
     case 'delete':
-      if (params.login === undefined) {
-        console.log('Вы ввели не правильную команду');
-        break;
-      };
-
-      const index = users.findIndex((user) => (user.login === params.login));
-      if (users[index] === -1) {
-        console.log('Такого пользователя нет');
-        break;
-      };
-
-      const {user} = getState();
-      if (users[index].login === user.login) {
-        console.log('нельзя удалить самого себя');
-        break;
-      };
-      console.log(`${users[index].login} удалён(ена)`);
-      users.splice(index, 1);
+      deleteCommand(params);
       break;
 
     case 'list':
-      if (params.take === undefined || params.skip === undefined) {
-        console.log('Вы ввели не правильную команду');
-        break;
-      };
-
-      const take = Number(params.take);
-      const skip = Number(params.skip);
-      if (Number.isNaN(take) || !Number.isInteger(take) || take <= 0) {
-        console.log('Параметр "take" должен быть целым положительным числом и должен быть больше нуля');
-        break;
-      };
-
-      if (Number.isNaN(skip) || !Number.isInteger(skip) || skip < 0) {
-        console.log('Параметр "take" должен быть целым неотрицательным числом и должен быть меньше илиравен нулю');
-        break;
-      };
-
-      console.table(users.slice(skip, take + skip));
-      
+      listCommand(params)
       break;
 
     case 'clear':
