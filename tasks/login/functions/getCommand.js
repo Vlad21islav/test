97bb012c308ea111b8d81a6736f83023a75a630e
Vlad21Islav <vmitk@mail.ru>
2023-07-14 11:11:14 +0300

    help

--- tasks/login/functions/getCommand.js
+++ tasks/login/functions/getCommand.js
@@ -29,12 +29,7 @@ function commands(line) {
       break;
 
     case 'help':
-      console.log(`1. add login=name password=password - команда добавления нового пользователя`);
-      console.log(`2. delete login=name - команда удаления пользователя (нельзя удалить самого себя)`);
-      console.log(`3. exit - выход`);
-      console.log(`4. help - выводит список всех команд`);
-      console.log(`4. list take=20 skip=10 - выводит список (c 11 по 20 строки)`);
-      console.log(`6. clear - отчистить консоль`);
+      listCommand();
       break;
 
     case 'exit':
