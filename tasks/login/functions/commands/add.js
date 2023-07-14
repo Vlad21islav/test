7d788e1630cc2e12a971e47ef63d5987d0c37235
Vlad21Islav <vmitk@mail.ru>
2023-07-14 11:18:02 +0300

    tub

--- tasks/login/functions/commands/add.js
+++ tasks/login/functions/commands/add.js
@@ -2,16 +2,16 @@ const users = require('./users');
 
 function addCommand(params) {
     if (params.login === undefined || params.password === undefined) {
-        console.log('Вы ввели не правильную команду');
-        return;
-      };
-      const findUser = users.find((element) => (element.login === params.login));
-      if (findUser !== undefined) {
-        console.log('Такой пользователь уже есть');
-        return;
-      };
-      users.push(params)
-      console.log(`${params.login} добавлен(a)`);
+      console.log('Вы ввели не правильную команду');
       return;
+    };
+    const findUser = users.find((element) => (element.login === params.login));
+    if (findUser !== undefined) {
+      console.log('Такой пользователь уже есть');
+      return;
+    };
+    users.push(params)
+    console.log(`${params.login} добавлен(a)`);
+    return;
 };
 module.exports = addCommand;
