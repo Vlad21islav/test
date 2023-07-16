7252790caa45819c6b04c5b9c356e9fbfd3c21f7
Vlad21Islav <vmitk@mail.ru>
2023-07-16 09:23:20 +0300

    function

--- tasks/reverse/reverse.js
+++ tasks/reverse/reverse.js
@@ -1,8 +1,6 @@
 'use strict';
-const logWords = require('./functions/logWords');
-const shuffleArray = require('./functions/shuffleArray');
-const {setState, getState} = require('./functions/variable');
 
+const { clear } = require('node:console');
 const readline = require('node:readline');
 
 const rl = readline.createInterface({
@@ -11,8 +9,45 @@ const rl = readline.createInterface({
   prompt: '──> ',
 });
 
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    };
+    return array;
+};
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), ms);
+  })
+} 
+
+function logWords() {
+  console.clear();
+  console.log('Запомните слово:');
+  console.log(words[word].split('').reverse().join(''));
+  sleep(3000).then(() => {
+    console.clear();
+    console.log('Введите слово правильно');
+    rl.prompt();
+    sleep(5000).then(() => {
+      if (isSleep) {
+        console.clear();
+        console.log(`время вышло, ваш рекорд ${word}`);
+        process.exit(0);
+      } else {
+        isSleep = true;
+      };
+    });
+  });
+};
+
 const words = shuffleArray(['привет', 'эскалатор', 'завтрак', 'тенис', 'телефон']);
-let {word} = getState();
+let word = 0;
+let isSleep = true;
 
 logWords();
 
@@ -20,8 +55,8 @@ rl.on('line', (line) => {
   if (word < words.length) {
     switch (line) {
       case words[word]:
-        setState({isSleep: false});
-        setState({word: word + 1});
+        isSleep = false;
+        word++;
         logWords();
         break;
 
