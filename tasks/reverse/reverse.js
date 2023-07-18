0ea34c75b4f4899595b6b366c913fea0c7ae470e
Vlad21Islav <vmitk@mail.ru>
2023-07-18 12:45:28 +0300

    bugs

--- tasks/reverse/reverse.js
+++ tasks/reverse/reverse.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const { clear } = require('node:console');
 const readline = require('node:readline');
 
 const rl = readline.createInterface({
@@ -10,61 +9,48 @@ const rl = readline.createInterface({
 });
 
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     };
     return array;
 };
 
-function sleep(ms) {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(), ms);
-  })
-} 
-
 function logWords() {
   console.clear();
   console.log('Запомните слово:');
-  console.log(words[word].split('').reverse().join(''));
-  sleep(3000).then(() => {
+  console.log(words[index].split('').reverse().join(''));
+  setTimeout(3000).then(() => {
     console.clear();
     console.log('Введите слово правильно');
     rl.prompt();
-    sleep(5000).then(() => {
-      if (isSleep) {
+    gameOver = setTimeout(5000).then(() => {
         console.clear();
-        console.log(`время вышло, ваш рекорд ${word}`);
+        console.log(`время вышло, ваш рекорд ${index}`);
         process.exit(0);
-      } else {
-        isSleep = true;
-      };
     });
   });
 };
 
 const words = shuffleArray(['привет', 'эскалатор', 'завтрак', 'тенис', 'телефон']);
-let word = 0;
-let isSleep = true;
+let index = 0;
 
 logWords();
 
 rl.on('line', (line) => {
-  if (word < words.length) {
-    switch (line) {
-      case words[word]:
-        isSleep = false;
-        word++;
-        logWords();
-        break;
-
-      default:
-        console.log(`Вы проиграли ваш рекорд ${word}`);
-        process.exit(0);
-    }
+  if (index < words.length) {
+    if (line === words[index]) {
+      clearTimeout(gameOver);
+      index++;
+      logWords();
+    } else {
+      console.log(`Вы проиграли ваш рекорд ${index}`);
+      process.exit(0);
+    };
   } else {
+    console.log(`Слова закончились, вы выиграли`);
     process.exit(0);
   };
 });
