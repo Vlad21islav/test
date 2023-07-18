9d01e454d305d62b7e6dfe976b20a59cad968efd
Vlad21Islav <vmitk@mail.ru>
2023-07-18 18:46:20 +0300

    time

--- tasks/reverse/reverse.js
+++ tasks/reverse/reverse.js
@@ -3,62 +3,68 @@
 const readline = require('node:readline');
 
 class Game {
-    constructor(words) {
-        this.words = this.shuffleWords(words);
-        this.index = 0
-        this.timeoutId;
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-            prompt: '──> '
-        });
-    };
+  constructor(words) {
+    this.words = this.shuffleWords(words);
+    this.overNum = 0
+    this.overTime = 0
+    this.index = 0
+    this.timeoutId;
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: '──> '
+    });
+  };
 
-    start() {
-        this.rl.on('line', (line) => {
-        if (this.index < words.length - 1) {
-            if (line === words[this.index]) {
-            clearTimeout(this.timeoutId);
-            this.index++;
-            this.next();
-            } else {
-            console.log(`Вы проиграли ваш рекорд ${this.index}`);
-            process.exit(0);
-            };
-        } else {
-            console.clear();
-            console.log(`Слова закончились, вы выиграли`);
-            process.exit(0);
-        };
-        });
-        this.next();
+  start() {
+    this.rl.on('line', (line) => {
+    if (this.index < this.words.length - 1) {
+      if (line === this.words[this.index]) {
+      clearTimeout(this.timeoutId);
+      this.index++;
+      this.next();
+      } else {
+      console.clear()
+      console.log(`Вы проиграли ваш рекорд ${this.index}, среднее время записи слова - ${this.overTime / this.overNum}, общее время - ${this.overTime}`);
+      process.exit(0);
+      };
+    } else {
+      console.clear();
+      console.log(`Слова закончились, вы выиграли, среднее время записи слова - ${this.overTime / this.overNum}, общее время - ${this.overTime}`);
+      process.exit(0);
     };
+    });
+    this.next();
+  };
 
-    next() {
+  next() {
+    console.clear();
+    console.log('Запомните слово:');
+    console.log(this.words[this.index].split('').reverse().join(''));
+    setTimeout(() => {
+      console.clear();
+      console.log('Введите слово правильно');
+      this.rl.prompt();
+      this.seconds = new Date().getSeconds();
+      this.timeoutId = setTimeout(() => {
         console.clear();
-        console.log('Запомните слово:');
-        console.log(words[this.index].split('').reverse().join(''));
-        setTimeout(() => {
-            console.clear();
-            console.log('Введите слово правильно');
-            this.rl.prompt();
-            this.timeoutId = setTimeout(() => {
-                console.clear();
-                console.log(`время вышло, ваш рекорд ${this.index}`);
-                process.exit(0);
-            }, 5000)
-        }, 3000)
-    }
+        console.log(`время вышло, ваш рекорд ${this.index}, среднее время записи слова - ${this.overTime / this.overNum}, общее время - ${this.overTime}`);
+        process.exit(0);
+      }, 5000)
+      this.overNum += 1;
+      this.overTime += new Date().getSeconds() - this.seconds;
+    }, 3000)
+  }
 
-    shuffleWords(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            let temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        };
-        return array;
+  shuffleWords(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     };
+    return array;
+  };
 };
 
 const russianWords = ['приветствие', 'забавный', 'красотка', 'победитель', 'интеллект', 'удивительный', 'надежный', 'эксперимент', 'мелодичный', 'восхитительный', 'растительность', 'демонстрация', 'громадный', 'оригинальный', 'совершенный', 'безопасность', 'инновационный', 'участник', 'эмоциональный', 'многообразие', 'исследование', 'высококачественный', 'образовательный', 'технологический', 'перспективный', 'увлекательный', 'симпатичный', 'настоящий', 'замечательный', 'оригинальность', 'разнообразие', 'креативный', 'эффективность', 'прогрессивный', 'необычный', 'стабильность', 'интересный', 'научный', 'современный', 'фантастический', 'уникальный', 'культурный', 'творческий', 'индивидуальность', 'продуктивность', 'экологический', 'безграничный', 'уверенность', 'просторный', 'гармоничный']
