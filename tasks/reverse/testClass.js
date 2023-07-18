ebac708422b4865987032ced17f6e0c117cf7bab
Vlad21Islav <vmitk@mail.ru>
2023-07-18 17:32:43 +0300

    testClass

--- tasks/reverse/testClass.js
+++ tasks/reverse/testClass.js
@@ -1,4 +1,65 @@
+'use strict';
+
+const readline = require('node:readline');
+
 class Game {
-    constructor(array) {}
-    start() {}
-}
+    constructor(words) {
+        this.words = words;
+        this.index = 0
+        this.timeoutId;
+        this.readline = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            prompt: '──> '
+        });
+    };
+
+    start() {
+        new Game(words).next();
+        this.rl.on('line', (line) => {
+        if (this.index < words.length - 1) {
+            if (line === words[this.index]) {
+            clearTimeout(timeoutId);
+            this.index++;
+            new Game(words).next();
+            } else {
+            console.log(`Вы проиграли ваш рекорд ${this.index}`);
+            process.exit(0);
+            };
+        } else {
+            console.clear();
+            console.log(`Слова закончились, вы выиграли`);
+            process.exit(0);
+        };
+        });
+    };
+
+    next() {
+        console.clear();
+        console.log('Запомните слово:');
+        console.log(words[this.index].split('').reverse().join(''));
+        setTimeout(() => {
+            console.clear();
+            console.log('Введите слово правильно');
+            rl.prompt();
+            timeoutId = setTimeout(() => {
+                console.clear();
+                console.log(`время вышло, ваш рекорд ${this.index}`);
+                process.exit(0);
+            }, 5000)
+        }, 3000)
+    }
+
+    shuffleWords(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        };
+        return array;
+    };
+};
+
+const words = ['привет', 'эскалатор', 'завтрак', 'тенис', 'телефон']
+new Game(words).start()
