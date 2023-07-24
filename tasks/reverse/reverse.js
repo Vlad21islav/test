111b681bf444db2a05a4c9b09849db941cd2f8bf
Vlad21Islav <vmitk@mail.ru>
2023-07-24 12:50:17 +0300

    env

--- tasks/reverse/reverse.js
+++ tasks/reverse/reverse.js
@@ -81,9 +81,9 @@ class Game {
 const russianWords = ['приветствие', 'забавный', 'красотка', 'победитель', 'интеллект', 'удивительный', 'надежный', 'эксперимент', 'мелодичный', 'восхитительный', 'растительность', 'демонстрация', 'громадный', 'оригинальный', 'совершенный', 'безопасность', 'инновационный', 'участник', 'эмоциональный', 'многообразие', 'исследование', 'высококачественный', 'образовательный', 'технологический', 'перспективный', 'увлекательный', 'симпатичный', 'настоящий', 'замечательный', 'оригинальность', 'разнообразие', 'креативный', 'эффективность', 'прогрессивный', 'необычный', 'стабильность', 'интересный', 'научный', 'современный', 'фантастический', 'уникальный', 'культурный', 'творческий', 'индивидуальность', 'продуктивность', 'экологический', 'безграничный', 'уверенность', 'просторный', 'гармоничный'];
 const englishWords = ['greeting ', 'funny ', 'beauty ', 'winner ', 'intelligence ', 'amazing ', 'reliable ', 'experiment ', 'melodic ', 'delightful ', 'vegetation ', 'demonstration ', 'enormous ', 'original ', 'perfect ', 'safety ', 'innovative ', 'participant ', 'emotional ', 'diversity ', 'research ', 'high-quality ', 'educational ', 'technological ', 'promising ', 'captivating ', 'cute ', 'genuine ', 'wonderful ', 'originality ', 'variety ', 'creative ', 'efficiency ', 'progressive ', 'unusual ', 'stability ', 'interesting ', 'scientific ', 'modern ', 'fantastic ', 'unique ', 'cultural ', 'creative ', 'individuality ', 'productivity ', 'ecological ', 'boundless ', 'confidence ', 'spacious ', 'harmonious'];
 
-if (LANG === 'rus' || LANG === 'rassian') {
+if (process.env.LANG === 'rus' || process.env.LANG === 'rassian') {
   new Game(russianWords).start();
-} else if (LANG === 'eng' || LANG ===  'english') {
+} else if (process.env.LANG === 'eng' || process.env.LANG ===  'english') {
   new Game(englishWords).start();
 } else {
   console.log('Введите в консоль "cross-env LANG=язык(рус / русскый, eng / english) node reverse.js"');
