661ba688774da8121396a42e39f518207fd8cba9
Vlad21Islav <vmitk@mail.ru>
2023-07-25 18:54:51 +0300

    bugs

--- tasks/reverse/Logger.js
+++ tasks/reverse/Logger.js
@@ -1,20 +1,32 @@
 'use strict';
-const words = require('./words')
 
 class Logger {
   constructor(massage) {
-    this.massage = massage
-    this.output = this.replace$1(massage, 'Vlad')
+    this.massage = massage;
+    this.output = this.replace$1('RECORD', 'Vlad');
   };
 
   replace$1(str, value) {
-    const otv = str.replace(`$1`, value);
-    if (otv === str) {
+    const otv = this.massage.str.replace(`$1`, value);
+    if (otv === this.massage.str) {
       console.log(str);
     } else {
       console.log(otv);
     };
   };
 };
- 
-new Logger('hello $1');
+
+const LANG = process.env.LANG !== 'rus' && process.env.LANG !== 'eng'
+  ? 'rus'
+  : process.env.LANG
+
+let massage;
+if (LANG === 'rus') {
+  massage = require('./langueges/rus');
+} else if (LANG === 'eng') {
+  massage = require('./langueges/eng');
+} else {
+  massage = require('./langueges/rus');
+};
+
+new Logger(massage);
--- tasks/reverse/langueges/rus.js
+++ tasks/reverse/langueges/rus.js
@@ -1,8 +1,8 @@
 module.exports = {
     YOU_HAVE_LOST: `Вы проиграли`,
-    RECORD: `ваш рекорд ${this.index}`,
-    EVERAGE_TIME: `среднее время записи слова - ${this.getAverageTime()} секунд`,
-    TOTAL_TIME: `общее время - ${Math.floor(this.overTime / 1000 * 100) / 100} секунд`,
+    RECORD: `ваш рекорд $1`,
+    EVERAGE_TIME: `среднее время записи слова - $1 секунд`,
+    TOTAL_TIME: `общее время - $1 секунд`,
     YOU_WON: `Слова закончились, вы выиграли`,
     REMEMBER_WORD: `Запомните слово:`,
     ENTER_WORD: `Введите слово правильно:`,
