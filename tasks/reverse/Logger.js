dae96ddbddcf8a4fa1fcdaa32f236b5b8690056d
Vlad21Islav <vmitk@mail.ru>
2023-07-26 17:58:27 +0300

    bugs

--- tasks/reverse/Logger.js
+++ tasks/reverse/Logger.js
@@ -8,9 +8,8 @@ class Logger {
   info(str, value) {
     if (this.massages[str] === undefined) {
       return(str);
-    } else {
-      return(this.massages[str].replace(`$1`, value));
     };
+    return(this.massages[str].replace(`$1`, value));
   };
 };
 module.exports = Logger;
