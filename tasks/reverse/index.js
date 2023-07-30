614e01c3e9660ab3fa703c608665dd9e15cbd8e6
Vlad21Islav <vmitk@mail.ru>
2023-07-30 14:07:50 +0300

    bugs

--- tasks/reverse/index.js
+++ tasks/reverse/index.js
@@ -2,17 +2,9 @@ const {words, messages} = require('./massages/index')
 const Logger = require('./Logger')
 const Game = require('./Game')
 
-let LANG = process.env.LANG !== 'rus' && process.env.LANG !== 'eng'
+const LANG = process.env.LANG !== 'rus' && process.env.LANG !== 'eng'
   ? 'rus'
   : process.env.LANG
-
-if (LANG === 'rus') {
-  LANG = 'rus'
-} else if (LANG === 'eng') {
-  LANG = 'eng'
-} else {
-  LANG = 'rus'
-};
 
 const logger =  new Logger(messages[LANG]) 
 const game = new Game(words[LANG], logger)
