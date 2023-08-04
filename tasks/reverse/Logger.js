daa65d51695a3fa890f0af14b60ae834a2732b4c
Vlad21Islav <vmitk@mail.ru>
2023-08-04 19:25:28 +0300

    Logger bugs

--- tasks/reverse/Logger.js
+++ tasks/reverse/Logger.js
@@ -7,9 +7,9 @@ class Logger {
 
   info(str, value) {
     let massage = this.get(str)
-    if (this.massages === undefined) return console.log(str)
+    if (this.massage === undefined) return console.log(str)
 
-    if (value === undefined) return console.log(this.massages)
+    if (value === undefined) return console.log(this.massage)
 
     value.forEach((value, index) => {
       massage = massage.replace(('$' + (index + 1)), value)
