e98ce9353b81290abcdcb1dfcd0b1093d6bdcc94
Vlad21Islav <vmitk@mail.ru>
2023-08-13 11:18:33 +0300

    circle2

--- tasks/circle2/circle2.js
+++ tasks/circle2/circle2.js
@@ -4,13 +4,16 @@ const calculateTheCircumference = (radius) => (2*Math.PI*radius);
 
 const calculateTheAreaOfTheCircle = (radius) => (Math.PI*(Math.pow(radius, 2)));
 
-rl.question('Введите радиус окружности в сантиметрах: ', (message) => {
+let message = document.getElementById('inp').innerHTML
+document.getElementById('inp').addEventListener('input', function() {
+
   const radius = Number(message);
   if (!Number.isNaN(radius)) {
     const circumference = calculateTheCircumference(radius);
     const area = calculateTheAreaOfTheCircle(radius);
-    console.log(`Площадь окружности: ${circumference.toFixed(2)} см.кв.`);
-    console.log(`Длина окружности: ${area.toFixed(2)} см.`);
-  }else console.log(`${message} - это не число`);
-  rl.close();
+    document.getElementById('Circumference').innerHTML = `Площадь окружности: ${circumference.toFixed(2)} см.кв.`;
+    document.getElementById('Area').innerHTML = `Длина окружности: ${area.toFixed(2)} см.`;
+  }else {
+    console.log(`${message} - это не число`);
+  };
 });
