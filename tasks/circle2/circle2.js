ed1bd79fcefa0eb7cd30eaa59c527bf1da2717d3
Vlad21Islav <vmitk@mail.ru>
2023-08-13 11:21:12 +0300

    circle2

--- tasks/circle2/circle2.js
+++ tasks/circle2/circle2.js
@@ -5,15 +5,13 @@ const calculateTheCircumference = (radius) => (2*Math.PI*radius);
 const calculateTheAreaOfTheCircle = (radius) => (Math.PI*(Math.pow(radius, 2)));
 
 let message = document.getElementById('inp').innerHTML
-document.getElementById('inp').addEventListener('input', function() {
 
-  const radius = Number(message);
-  if (!Number.isNaN(radius)) {
-    const circumference = calculateTheCircumference(radius);
-    const area = calculateTheAreaOfTheCircle(radius);
-    document.getElementById('Circumference').innerHTML = `Площадь окружности: ${circumference.toFixed(2)} см.кв.`;
-    document.getElementById('Area').innerHTML = `Длина окружности: ${area.toFixed(2)} см.`;
-  }else {
-    console.log(`${message} - это не число`);
-  };
-});
+const radius = Number(message);
+if (!Number.isNaN(radius)) {
+  const circumference = calculateTheCircumference(radius);
+  const area = calculateTheAreaOfTheCircle(radius);
+  document.getElementById('Circumference').innerHTML = `Площадь окружности: ${circumference.toFixed(2)} см.кв.`;
+  document.getElementById('Area').innerHTML = `Длина окружности: ${area.toFixed(2)} см.`;
+}else {
+  console.log(`${message} - это не число`);
+};
