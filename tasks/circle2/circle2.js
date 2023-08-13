4812d1aff43c88ff14fb4168364ee44c95215ce1
Vlad21Islav <vmitk@mail.ru>
2023-08-13 19:31:00 +0300

    circle2

--- tasks/circle2/circle2.js
+++ tasks/circle2/circle2.js
@@ -1,18 +1,16 @@
-'use strict';
-
 const calculateTheCircumference = (radius) => (2*Math.PI*radius);
 
 const calculateTheAreaOfTheCircle = (radius) => (Math.PI*(Math.pow(radius, 2)));
 
-let message = document.getElementById('inp').innerHTML
-
-const radius = Number(message);
-if (!Number.isNaN(radius)) {
-  const circumference = calculateTheCircumference(radius);
-  const area = calculateTheAreaOfTheCircle(radius);
-  document.getElementById('Circumference').innerHTML = `Площадь окружности: ${circumference.toFixed(2)} см.кв.`;
-  document.getElementById('Area').innerHTML = `Длина окружности: ${area.toFixed(2)} см.`;
-}else {
-  document.getElementById('Circumference').innerHTML = `${message} - это не число`;
-  document.getElementById('Area').innerHTML = ``;
-};
+document.getElementById('number').addEventListener('input', function() {
+  const radius = Number(this.value);
+  if (!Number.isNaN(radius)) {
+    const circumference = calculateTheCircumference(radius);
+    const area = calculateTheAreaOfTheCircle(radius);
+    document.getElementById('otv1').innerHTML = `Площадь окружности: ${area.toFixed(2)} см.кв.`;
+    document.getElementById('otv2').innerHTML = `Длина окружности: ${circumference.toFixed(2)} см.`;
+  }else {
+    document.getElementById('otv1').innerHTML = `${this.value} - это не число`;
+    document.getElementById('otv2').innerHTML = ``;
+  };
+});
