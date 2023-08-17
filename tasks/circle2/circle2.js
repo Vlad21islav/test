4840428edbf1358203a87a6c8ef3ab3e49d0ded5
Vlad21Islav <vmitk@mail.ru>
2023-08-17 21:55:03 +0300

    bugs

--- tasks/circle2/circle2.js
+++ tasks/circle2/circle2.js
@@ -10,26 +10,26 @@ const calculateTheAreaOfTheCircle = (radius) => (Math.PI*(Math.pow(radius, 2)));
 
 if (number === null || otv1 === null || otv2 === null || btn  === null || errors === null) {
   console.log('нет одного из элементов');
-};
-
-btn.addEventListener('click', function() {
-  number.value = ``; 
-  otv1.innerHTML = `The area of the circle: 0.00 sm.sq.`; 
-  otv2.innerHTML = `Circumference length: 0.00 sm.`;
-  errors.innerHTML = '';
-});
-
-number.addEventListener('input', function(event) {
-  const radius = Number(event.target.value);
-  if (!Number.isNaN(radius)) {
-    const circumference = calculateTheCircumference(radius);
-    const area = calculateTheAreaOfTheCircle(radius);
-    otv1.innerHTML = `The area of the circle: ${area.toFixed(2)} sm.sq.`;
-    otv2.innerHTML = `Circumference length: ${circumference.toFixed(2)} sm.`;
+} else {
+  btn.addEventListener('click', function() {
+    number.value = ``; 
+    otv1.innerHTML = `The area of the circle: 0.00 sm.sq.`; 
+    otv2.innerHTML = `Circumference length: 0.00 sm.`;
     errors.innerHTML = '';
-  }else {
-    otv1.innerHTML = '';
-    otv2.innerHTML = '';
-    errors.innerHTML = `${event.target.value} - isn't a number`;
-  };
-});
+  });
+
+  number.addEventListener('input', function(event) {
+    const radius = Number(event.target.value);
+    if (!Number.isNaN(radius)) {
+      const circumference = calculateTheCircumference(radius);
+      const area = calculateTheAreaOfTheCircle(radius);
+      otv1.innerHTML = `The area of the circle: ${area.toFixed(2)} sm.sq.`;
+      otv2.innerHTML = `Circumference length: ${circumference.toFixed(2)} sm.`;
+      errors.innerHTML = '';
+    }else {
+      otv1.innerHTML = '';
+      otv2.innerHTML = '';
+      errors.innerHTML = `${event.target.value} - isn't a number`;
+    };
+  });
+}
